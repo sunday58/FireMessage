9d1a6bdc67530526b19f6df4151a550a5bf35d13
sundaydavid <sundaydavid989@gmail.com>
2020-07-05 09:48:16 +0100

    creating image message resource file

--- app/src/main/java/com/sundaydavid/firemessage/model/ImageMessage.kt
+++ app/src/main/java/com/sundaydavid/firemessage/model/ImageMessage.kt
@@ -1,4 +1,11 @@
 package com.sundaydavid.firemessage.model
 
-class ImageMessage {
+import java.util.*
+
+data class ImageMessage(val imagePath: String,
+                       override val time: Date,
+                       override  val senderId: String,
+                       override val type: String = MessageType.IMAGE)
+    : Message {
+    constructor() : this("", Date(0), "" )
 }
